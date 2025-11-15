5c5254474f009234b8dbb04dad92fc3caf4d08e2
Exityx <gdlasorum@gmail.com>
2025-11-15 18:40:52 +0300

    feat(parser): Created implementations for class ProductLogParser

---
+++ product_log_parser.cpp
@@ -0,0 +1,40 @@
+#include "product_log_parser.h"
+
+Product ProductLogParser::parse_product()
+{
+    std::string t1;
+    int t2, t3, t4;
+    file >> t1 >> t2 >> t3 >> t4;
+    return Product(t1, t2, t3, t4);
+}
+
+Product ProductLogParser::parse_product(const std::string &product_name)
+{
+    std::ifstream local_thread;
+    std::string t1;
+    int t2, t3, t4;
+    local_thread.open(file_name_m);
+    while (local_thread >> t1 >> t2 >> t3 >> t4)
+    {
+        if (t1 == product_name)
+        {
+            local_thread.close();
+            return Product(t1, t2, t3, t4);
+        }   
+    }
+    local_thread.close();
+    return Product("Not found product '" + product_name + "'", 0, 0, 0);
+}
+
+std::vector<Product> ProductLogParser::parse_all_file()
+{
+    std::ifstream local_thread;
+    std::vector<Product> result;
+    std::string t1;
+    int t2, t3, t4;
+    local_thread.open(file_name_m);
+    while (local_thread >> t1 >> t2 >> t3 >> t4)
+        result.push_back(Product(t1,t2,t3,t4));
+    local_thread.close();
+    return result;
+}
