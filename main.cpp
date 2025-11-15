44dfd23ba9d69b7890415fdf8c5ea8fdc7efbbf8
Exityx <gdlasorum@gmail.com>
2025-11-15 16:28:53 +0300

    fix: Add usage Product and ProductLogging classes

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include "product.h"
+#include "product_logging.h"
 
 int main(void)
 {
     using namespace std;
+    ProductLogging logger("log.txt");
+    Product milk("Milk", 120, 25, 10);
+    Product bread("Bread", 35, 40, 12);
+    Product sugar("Sugar", 280, 10, 2);
 
+    logger.log_product(milk);
+    logger.log_product(bread);
+    logger.log_product(sugar);
     return 0;
 }
